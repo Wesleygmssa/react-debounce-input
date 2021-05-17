9c79bf324153101c18378f01eece5da02db5b603
wesley guerra <wesleyguerra9@gmail.com>
2021-05-17 11:09:04 -0300

    ClearProject

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { DebounceInput } from "react-debounce-input";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    value: "",
+  };
+
+  render() {
+    return (
+      <div>
+        <DebounceInput
+          minLength={3}
+          debounceTimeout={300}
+          onChange={(event) => this.setState({ value: event.target.value })}
+        />
+
+        <p>Value: {this.state.value}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+// const appRoot = document.createElement("div");
+// document.body.appendChild(appRoot);
+// ReactDOM.render(<App />, appRoot);
